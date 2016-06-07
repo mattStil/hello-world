54c2b99480c0b0e67cbd12e39f8e556c739c78c4
mattStil <matthewstillion2@gmail.com>
2016-06-07 17:45:44 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -8,10 +8,22 @@ window.onload = function() {
         jobs:[
           farmer = {
             name: 'farmer',
-            salary: 2
+            salary: 2,
+            buildings: ['farm'],
           },
+          worker = {
+            name: "worker",
+            salary: 2,
+            buildings: ['mine','quarry','mill'],
+          },
+          researcher = {
+            name: 'researcher',
+            salary:3,
+            buildings: ['lab']
+          }
         ],
-        houses:100,
+        houses:1000,
+        needed: 0,
       };
       Object.prototype.addToPop = function(obj)  {
         population.array[population.array.length] = obj;
@@ -25,14 +37,16 @@ window.onload = function() {
       };
       //builds the people if the population is less than needed
       popDeficeit = function() {
+        clearInterval(interval);
         if (population.array.length < population.houses) {
           population.array[population.array.length] = new Person();
-        }
-        population.needed = population.houses - population.array.length
+        };
+        population.needed = population.houses - (population.array.length - 1);
+        interval = setInterval(popDeficeit, Math.floor((10000/population.needed) + 1));
       };
 
       popG = function() {
-        setInterval(popDeficeit,(1000/(population.needed)+1));
+        interval = setInterval(popDeficeit, Math.floor((10000/(population.needed)+1)));
       };
       popG();
 
@@ -51,30 +65,35 @@ window.onload = function() {
       eff: 0.25,
       production: 1,
       store:0,
-      num:0
+      notStore:0,
+      num:0,
       },
       mine: {
         eff: 0.25,
         production:1,
         store:0,
+        notStore:0,
         num:0,
       },
       quarry:{
         eff:0.25,
         production:1,
         store:0,
+        notStore:0,
         num:0
       },
       mill: {
         eff:0.15,
         production:0.95,
         store:0,
+        notStore:0,
         num:0
       },
       pres: {
         eff:1,
         production:1,
         store:500,
+        notStore:0,
         num:0,
       }
     };
@@ -84,15 +103,43 @@ window.onload = function() {
   production();
   //eco is to add all the production of the farms and such to the stores
   eco = function() {
+    //calculates the production for the various
+    produ = function(building) {
+      pro.building.notStore += (pro.building.num * pro.building.eff);
+    }
+    move = function() {
+      pro.building.store++
+    };
     income = function() {
-      pro.farm.store += (pro.farm.num * pro.farm.eff);
-      pro.mine.store += (pro.mine.num * pro.mine.eff);
-      pro.quarry.store += (pro.quarry.num * pro.quarry.eff);
-      pro.mill.store += (pro.mill.num * pro.mill.eff);
-      pro.pres.store += (pro.pres.num * pro.pres.eff);
+      produ(farm);
+      produ(mine);
+      produ(mill);
+      produ(quarry);
+      produ(press);
     };
 
 
     setInterval(income,1000);
   }
+  //function for different job types
+  countJob = function(job, id2) {
+    job = 0;
+    for ( var i = 0; i < population.array.length; i++) {
+        if(population.array[i].job.name === id2) {
+          job++;
+        };
+    };
+    document.getElementById((id2+'s')).innerHTML = job;
+  };
+  //keeps track of the num of types
+  jobCounting = function() {
+    setInterval(function() {
+      countJob(worker,'worker');
+      countJob(farmer,'farmer');
+      countJob(researcher,'researcher');
+      totalPop = population.array.length;
+      document.getElementById('pop').innerHTML = totalPop;
+    },100);
+  };
+  jobCounting();
 };
---
+++ test.py
@@ -0,0 +1,12 @@
+import datetime from datetime
+now = datetime.now()
+
+print now
+
+my_variable = 0
+
+def clinic();
+    response = raw_input('What is your name?')
+
+    if response != None:
+    print response
